7f2494868a406380fcf5e3714ca7eee949285ef2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-01-03 19:18:00 +0100

    Added new endpoints

--- api/DbManager.go
+++ api/DbManager.go
@@ -4,38 +4,31 @@ import "gopkg.in/mgo.v2"
 
 type DbManager struct {
 	Database string
-	Table string
 }
 
 func NewDbManager() *DbManager{
-	return &DbManager{"gameRanking", "results"}
+	return &DbManager{"gameRanking"}
 }
 
-func (mgr DbManager) Insert(msg *Result) {
+func (mgr DbManager) Insert(msg *Result, table string) {
 	session, err := mgo.Dial(*DbHost)
 	failOnError(err, "Unable to connect to MongoDB")
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB(mgr.Database).C(mgr.Table)
+	c := session.DB(mgr.Database).C(table)
 	err = c.Insert(msg)
 	failOnError(err, "Unable to insert to database")
 
 	defer session.Close()
 }
 
-func (mgr DbManager) Receive(limit int) []Result {
+func (mgr DbManager) Receive(limit int, table string, primarySort string, secondarySort string) []Result {
 	session, err := mgo.Dial(*DbHost)
 	failOnError(err, "Unable to connect to MongoDB")
 
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
 	var msg []Result
 
-	c := session.DB(mgr.Database).C(mgr.Table)
-	err = c.Find(nil).Sort("-score", "time").Limit(limit).All(&msg)
+	c := session.DB(mgr.Database).C(table)
+	err = c.Find(nil).Sort(primarySort, secondarySort).Limit(limit).All(&msg)
 
 	failOnError(err, "Unable to select from database")
 
